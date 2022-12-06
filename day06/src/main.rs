6b78fdb4d176f50649fd527a76dab46e3ce95574
Eugene Chou <euchou@ucsc.edu>
2022-12-06 10:30:30 -0800

    dry

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -1,41 +1,29 @@
 use anyhow::Result;
 use itertools::Itertools;
 
+fn find_message(stream: &[char], marker_size: usize) -> String {
+    stream
+        .windows(marker_size)
+        .enumerate()
+        .find_map(|(i, window)| {
+            (window
+                .iter()
+                .map(|c| (1 << (*c as u32 - 'a' as u32)) as u32)
+                .fold(0, |a, c| a | c)
+                .count_ones()
+                == marker_size as u32)
+                .then_some(i + marker_size)
+        })
+        .unwrap()
+        .to_string()
+}
+
 fn main() -> Result<()> {
     let input = std::fs::read_to_string("input.txt")?;
-    let chars = input.chars().collect_vec();
+    let stream = input.chars().collect_vec();
 
-    println!(
-        "part 1: {}",
-        chars
-            .windows(4)
-            .enumerate()
-            .find_map(|(i, w)| (w
-                .iter()
-                .map(|c| (1 << (u32::from(*c) - 'a' as u32)) as usize)
-                .fold(0, |a, c| a | c)
-                .count_ones()
-                == 4)
-                .then_some(i + 4))
-            .unwrap()
-            .to_string()
-    );
-
-    println!(
-        "part 2: {}",
-        chars
-            .windows(14)
-            .enumerate()
-            .find_map(|(i, w)| (w
-                .iter()
-                .map(|c| (1 << (u32::from(*c) - 'a' as u32)) as usize)
-                .fold(0, |a, c| a | c)
-                .count_ones()
-                == 14)
-                .then_some(i + 14))
-            .unwrap()
-            .to_string()
-    );
+    println!("part 1: {}", find_message(&stream, 4));
+    println!("part 2: {}", find_message(&stream, 14));
 
     Ok(())
 }
