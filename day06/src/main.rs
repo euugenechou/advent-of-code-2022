bf50ec141d8391135036e717f7d84f69862ce3c8
Eugene Chou <euchou@ucsc.edu>
2022-12-06 02:47:10 -0800

    who needs a `HashSet`

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -1,6 +1,5 @@
 use anyhow::Result;
 use itertools::Itertools;
-use std::collections::HashSet;
 
 fn main() -> Result<()> {
     let input = std::fs::read_to_string("input.txt")?;
@@ -11,17 +10,31 @@ fn main() -> Result<()> {
         chars
             .windows(4)
             .enumerate()
-            .find_map(|(i, w)| (w.iter().collect::<HashSet<_>>().len() == 4).then_some(i + 4))
+            .find_map(|(i, w)| (w
+                .iter()
+                .map(|c| (1 << (u32::from(*c) - 'a' as u32)) as usize)
+                .reduce(|a, c| a | c)
+                .unwrap()
+                .count_ones()
+                == 4)
+                .then_some(i + 4))
             .unwrap()
             .to_string()
     );
 
     println!(
-        "part 2: {}",
+        "part 1: {}",
         chars
             .windows(14)
             .enumerate()
-            .find_map(|(i, w)| (w.iter().collect::<HashSet<_>>().len() == 14).then_some(i + 14))
+            .find_map(|(i, w)| (w
+                .iter()
+                .map(|c| (1 << (u32::from(*c) - 'a' as u32)) as usize)
+                .reduce(|a, c| a | c)
+                .unwrap()
+                .count_ones()
+                == 14)
+                .then_some(i + 14))
             .unwrap()
             .to_string()
     );
